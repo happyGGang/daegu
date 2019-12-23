0009f48e2fec62700bf80f44eed034331bbb4ffb
YONGJU <odetsuo@gmail.com>
2019-12-23 10:04:01 +0900

    중앙, 동부 리소스 추가

---
+++ src/main/webapp/resources/homepage/jungang/js/common.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+	pageMain.init();
+});
+
+
+var pageMain = (function(){
+	var init, bindEvent;
+
+	init = function() {
+		bindEvent();
+	};
+
+	bindEvent = function(){
+
+		// TAB
+		$(document).on('click', '.tabMenuS a', function(){
+			var target = this.getAttribute('href').replace('#','');
+			var $box = $(this).closest('.tabS');
+			var moreUrl = $(this).data('link');
+
+			$(this).closest('.tabMenuS').find('li').removeClass('on');
+			$(this).parent('li').addClass('on');
+
+			$box.find('.con').hide();
+			$box.find('[data-tab="'+target+'"]').show();
+			$box.find('.more-more').attr('href', moreUrl);
+
+		});
+
+	};
+
+	return {
+		init: init
+	}
+})();
+
+$(function(){
+	
+	// 팝업존(중앙도서관)
+	if ($('.popZone ul').length > 0) {
+		$('.popZone ul').bxSlider({
+			mode:'fade',
+			pager: true,
+			pagerType: 'short',
+			auto: true,
+			autoControls: true,
+			autoControlsCombine: true
+		});
+	}
+
+});
