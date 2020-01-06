84ae2d6e62e5e16c5f6dc87143e95b50ce826d57
YONGJU <odetsuo@gmail.com>
2020-01-06 14:43:58 +0900

    검색데 응답헤더 추가

--- src/main/java/kr/go/gbelib/app/intro/IntroController.java
+++ src/main/java/kr/go/gbelib/app/intro/IntroController.java
@@ -1,6 +1,7 @@
 package kr.go.gbelib.app.intro;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,13 +13,22 @@ import kr.co.whalesoft.framework.base.BaseController;
 @Controller
 @RequestMapping(value = {"/intro"})
 public class IntroController extends BaseController {
-	
+
 	private final String basePath = "/intro/";
-	
+
 	@RequestMapping(value = {"/{context_path}/index.*"})
-	public String index(@PathVariable String context_path, Model model, HttpServletRequest request) {
+	public String index(@PathVariable String context_path, Model model, HttpServletRequest request, HttpServletResponse response) {
+		response.setHeader("X-Frame-Options", "DENY");
+		response.setHeader("X-Content-Type-Options", "nosniff");
+		response.setHeader("X-XSS-Protection", "1");
+		response.setHeader("Cache-Control", "no-store");
+		response.setHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", 0);
+		if (request.getProtocol().equals("HTTP/1.1")) {
+			response.setHeader("Cache-Control", "no-cache");
+		}
 		model.addAttribute("homepage", request.getAttribute("homepage"));
 		return basePath + "index";
 	}
-	
+
 }
