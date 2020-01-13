0788e5097fbc2f42cc6b011c2de3a91e5f03d2c9
YONGJU <odetsuo@gmail.com>
2020-01-13 09:14:08 +0900

    퀵메뉴 태그 오류 수정

--- src/main/java/kr/co/whalesoft/framework/tag/homepage/QuickMenuTag.java
+++ src/main/java/kr/co/whalesoft/framework/tag/homepage/QuickMenuTag.java
@@ -5,13 +5,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 
 import org.apache.commons.lang.StringUtils;
 
-import kr.co.whalesoft.app.cms.homepage.Homepage;
 import kr.co.whalesoft.app.cms.quickMenu.QuickMenu;
 import kr.co.whalesoft.framework.tag.HtmlTag;
 
@@ -19,24 +17,23 @@ public class QuickMenuTag extends BodyTagSupport {
 
 	private static final long serialVersionUID = 1L;
 	private List<QuickMenu> quickMenuList;
-	
+
 	@Override
 	public int doEndTag() throws JspException {
-		HttpServletRequest request = (HttpServletRequest)pageContext.getRequest();
-		Homepage homepage = (Homepage)request.getAttribute("homepage");
 		int count = 0;
 		if ( getQuickMenuList() != null ) {
 			for ( QuickMenu one : getQuickMenuList() ) {
-				
+
 				count = count+1;
 				HtmlTag li_tag 		= new HtmlTag("li");
-				HtmlTag div_tag		= new HtmlTag("div");
 				HtmlTag a_tag 		= new HtmlTag("a");
 				HtmlTag span_tag 	= new HtmlTag("span");
 
 				String link_url = "";
 				if ( StringUtils.isEmpty(one.getLink_url())) {
 					link_url = "javascript:alert('준비중입니다.');";
+				} else {
+					link_url = one.getLink_url();
 				}
 				if ( StringUtils.isNotEmpty(one.getLink_target()) && one.getLink_target().equals("BLANK") ) {
 					a_tag.setAttribute("target", "_blank");
@@ -46,12 +43,7 @@ public class QuickMenuTag extends BodyTagSupport {
 				li_tag.setAttribute("class", "qm"+count);
 				span_tag.setContent(one.getMenu_name());
 				a_tag.addSubTag(span_tag);
-				if( homepage.getHomepage_id().equals("h17")){
-					div_tag.addSubTag(a_tag);
-					li_tag.addSubTag(div_tag);
-				} else {
-					li_tag.addSubTag(a_tag);
-				}
+				li_tag.addSubTag(a_tag);
 
 				try {
 					pageContext.getOut().println(li_tag.toString());
@@ -61,7 +53,7 @@ public class QuickMenuTag extends BodyTagSupport {
 				}
 			}
 		}
-		
+
 		return EVAL_PAGE;
 	}
 
