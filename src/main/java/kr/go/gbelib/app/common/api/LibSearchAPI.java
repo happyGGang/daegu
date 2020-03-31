fe730aaca7b9a2cbe4797420eea87c077dd3406f
YONGJU <odetsuo@gmail.com>
2020-03-31 15:11:57 +0900

    야간예약 - 대출만기일수 추가

--- src/main/java/kr/go/gbelib/app/common/api/LibSearchAPI.java
+++ src/main/java/kr/go/gbelib/app/common/api/LibSearchAPI.java
@@ -960,6 +960,7 @@ public class LibSearchAPI {
 		}
 		param.put("booktype", booktype);// 자료타입 BO:단행본, SE:연속간행물
 		param.put("worker", librarySearch.getWorker());// 장비ID
+		param.put("expire_date_cnt", librarySearch.getExprire_date_cnt());// 예약만기일수 -입력시 예약만기일 : API 서비스 요청 당일+예약만기일수. -미입력시 예약만기일 : 당일
 
 		Map<String, Object> sendKCMS = CommonAPI.sendKCMS("nightloanreserve", param);
 
