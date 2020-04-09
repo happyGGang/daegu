d9d86f76e55ddcaa2506123838062c262a780be1
YONGJU <odetsuo@gmail.com>
2020-04-09 15:00:52 +0900

    야간대출예약(워킹스루) 제한 추가

--- src/main/java/kr/go/gbelib/app/intro/search/CommonSearchController.java
+++ src/main/java/kr/go/gbelib/app/intro/search/CommonSearchController.java
@@ -67,7 +67,7 @@ public class CommonSearchController extends BaseController {
 
 	@Autowired
 	private SmsReceptionService smsReceptionService;
-	
+
 	@Autowired
 	private NewBookConfigService newBookConfigService;
 
@@ -594,12 +594,12 @@ public class CommonSearchController extends BaseController {
 			if(code_arr == null) {
 				break;
 			}
-			
+
 			if(code_arr.contains(map.get("CODE"))) {
 				map.put("CHECKED", true);
 			}
 		}
-		
+
 		model.addAttribute("shelfList", shelfList);
 
 		if (StringUtils.isEmpty(librarySearch.getShelfCode())) {
@@ -1721,6 +1721,30 @@ public class CommonSearchController extends BaseController {
 				res.setMessage(lasReqConfig.getRes_msg());
 				return res;
 			}
+
+			Map<String, Object> nightLoanReserveCnt = LibSearchAPI.getNightLoanReserveCnt(librarySearch, "USER");
+			String nightLoanResult = String.valueOf(nightLoanReserveCnt.get("RESULT_INFO"));
+			if (StringUtils.equals(nightLoanResult, "SUCCESS")) {
+				String limit_cnt = String.valueOf(nightLoanReserveCnt.get("TOTAL"));
+				try {
+					int limit_count = Integer.parseInt(limit_cnt);
+					if (limit_count >= 50) {
+						res.setValid(false);
+						res.setMessage("해당 도서관의 금일 워킹스루 예약가능 인원이 모두 찼습니다. 내일 다시 신청해주세요");
+						return res;
+					}
+				} catch (Exception e) {
+					res.setValid(false);
+					res.setMessage("해당 도서관의 금일 워킹스루 예약가능 인원이 모두 찼습니다. 에러코드 060");
+					return res;
+				}
+			} else {
+				res.setValid(false);
+				res.setMessage(String.valueOf(nightLoanReserveCnt.get("RESULT_MESSAGE")));
+				return res;
+			}
+
+
 
 			librarySearch.setUserkey(member.getRec_key());
 			ApiResponse apiResult = LibSearchAPI.nightloanreserve(librarySearch);
