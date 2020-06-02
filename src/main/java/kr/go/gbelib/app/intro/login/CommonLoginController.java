3b79da2c5e330051e9e126ba7e5b1d3edf5c633a
dseok63 <dseok63@gmail.com>
2020-06-02 11:33:58 +0900

    로그인 사용자 권한 오류 수정

--- src/main/java/kr/go/gbelib/app/intro/login/CommonLoginController.java
+++ src/main/java/kr/go/gbelib/app/intro/login/CommonLoginController.java
@@ -142,8 +142,10 @@ public class CommonLoginController extends BaseController {
 					if (member.getAuthGroupIdxList() == null || member.getAuthGroupIdxList().size() < 1) {
 						member.setAuthGroupIdxList(new ArrayList<Integer>());
 
-						//관리자 링크회원이면 기존 그룹에 추가
-						member.setAuthGroupIdxList(memberGroupSubordService.getAuthGroupIdxList(adminMember));
+//						관리자 링크회원이면 기존 그룹에 추가
+						if(adminMember != null) {
+							member.setAuthGroupIdxList(memberGroupSubordService.getAuthGroupIdxList(adminMember));
+						}
 						//통합회원그룹에 속하게 한다. 도서관은 하드코딩한다...
 						if (!member.getAuthGroupIdxList().contains(3)) {
 							member.getAuthGroupIdxList().add(3);
