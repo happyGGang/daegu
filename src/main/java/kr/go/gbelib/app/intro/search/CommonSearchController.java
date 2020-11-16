967c594dd0c92e3faaa2d661bbd2599d6672c697
YONGJU <odetsuo@gmail.com>
2020-11-16 16:57:28 +0900

    신착자료 자료실 없는 경우 예외처리 추가

--- src/main/java/kr/go/gbelib/app/intro/search/CommonSearchController.java
+++ src/main/java/kr/go/gbelib/app/intro/search/CommonSearchController.java
@@ -594,19 +594,23 @@ public class CommonSearchController extends BaseController {
 
 
 		Map<String, Object> subLocaInfo = LibSearchAPI.getSubLocaInfo("19", homepage.getManage_code());
-		List<Map<String, Object>> shelfList = LibSearchAPI.getListData(subLocaInfo, "LIST_DATA");
-		List<String> code_arr = newBookConfigService.getShelfCodeList(new NewBookConfig(homepage.getHomepage_id()));
-		for (Map<String, Object> map : shelfList) {
-			if(code_arr == null) {
-				break;
+		if (!"ERROR".equals(subLocaInfo.get("RESULT_INFO"))) {
+			List<Map<String, Object>> shelfList = LibSearchAPI.getListData(subLocaInfo, "LIST_DATA");
+
+			List<String> code_arr = newBookConfigService.getShelfCodeList(new NewBookConfig(homepage.getHomepage_id()));
+			for (Map<String, Object> map : shelfList) {
+				if(code_arr == null) {
+					break;
+				}
+
+				if(code_arr.contains(map.get("CODE"))) {
+					map.put("CHECKED", true);
+				}
 			}
 
-			if(code_arr.contains(map.get("CODE"))) {
-				map.put("CHECKED", true);
-			}
+			model.addAttribute("shelfList", shelfList);
 		}
 
-		model.addAttribute("shelfList", shelfList);
 
 		if (StringUtils.isEmpty(librarySearch.getShelfCode())) {
 			librarySearch.setShelfCode("ALL");
