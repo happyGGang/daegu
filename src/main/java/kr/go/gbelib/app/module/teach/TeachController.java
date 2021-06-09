cb4427cbaf94850b33f6a57d26d693b2df13270c
YONGJU <odetsuo@gmail.com>
2021-06-09 14:35:01 +0900

    수강신청 목록 용학 서브도서관 처리 누락 추가

--- src/main/java/kr/go/gbelib/app/module/teach/TeachController.java
+++ src/main/java/kr/go/gbelib/app/module/teach/TeachController.java
@@ -218,7 +218,7 @@ public class TeachController extends BaseController{
 		else {
 //			teach.setHomepage_id(homepage.getHomepage_id());
 			if ((homepage.getHomepage_id().equals("h37") || homepage.getHomepage_id().equals("h49") || homepage.getHomepage_id().equals("h45") || homepage.getHomepage_id().equals("h53"))
-					|| homepage.getHomepage_id().equals("h50")) {
+					|| homepage.getHomepage_id().equals("h50") || homepage.getHomepage_id().equals("h51")) {
 				Homepage h = new Homepage();
 				h.setHomepage_id(homepage.getHomepage_id());
 				h.setHomepage_group(homepage.getHomepage_id());
@@ -226,7 +226,7 @@ public class TeachController extends BaseController{
 				List<Homepage> subHomepageList = homepageService.getSubHomepageList(h);
 				if (StringUtils.isEmpty(teach.getHomepage_id())) {
 					if (subHomepageList != null && subHomepageList.size() > 0) {
-						if(homepage.getHomepage_id().equals("h50")) {
+						if(homepage.getHomepage_id().equals("h50") || homepage.getHomepage_id().equals("h51")) {
 							teach.setHomepage_id(homepage.getHomepage_id());
 						} else {
 							teach.setHomepage_id(subHomepageList.get(0).getHomepage_id());
