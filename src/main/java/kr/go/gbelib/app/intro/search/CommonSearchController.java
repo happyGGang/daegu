8b91f4ee577da46bdb5c1dc8b419e4134c841867
김성현 <ttkazuya1@gmail.com>
2022-01-10 14:44:32 +0900

    안심도서관 워킹스루 전체건수 수정

--- src/main/java/kr/go/gbelib/app/intro/search/CommonSearchController.java
+++ src/main/java/kr/go/gbelib/app/intro/search/CommonSearchController.java
@@ -2342,10 +2342,18 @@ public class CommonSearchController extends BaseController {
 				String limit_cnt = String.valueOf(nightLoanReserveCnt.get("TOTAL"));
 				try {
 					int limit_count = Integer.parseInt(limit_cnt);
-					if (limit_count >= 100) {
-						res.setValid(false);
-						res.setMessage("해당 도서관의 금일 워킹스루 예약가능 인원이 모두 찼습니다. 내일 다시 신청해주세요");
-						return res;
+					if(homepage.getHomepage_id().equals("h73")) {
+						if (limit_count >= 600) {
+							res.setValid(false);
+							res.setMessage("해당 도서관의 금일 워킹스루 예약가능 인원이 모두 찼습니다. 내일 다시 신청해주세요");
+							return res;
+						}
+					} else {
+						if (limit_count >= 100) {
+							res.setValid(false);
+							res.setMessage("해당 도서관의 금일 워킹스루 예약가능 인원이 모두 찼습니다. 내일 다시 신청해주세요");
+							return res;
+						}
 					}
 				} catch (Exception e) {
 					res.setValid(false);
