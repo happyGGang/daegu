da40f1bcca46eabf02c2f20c965de256e9b36291
YunHaesu <Yunhaesu.dev@gmail.com>
2022-01-24 14:17:46 +0900

    동구통합도서관 희망도서 제한 주석처리

--- src/main/java/kr/go/gbelib/app/intro/search/CommonSearchController.java
+++ src/main/java/kr/go/gbelib/app/intro/search/CommonSearchController.java
@@ -952,10 +952,10 @@ public class CommonSearchController extends BaseController {
 		Homepage homepage = getSessionHomepage(request);
 		
 		// 대구동구통합도서관 예산초과로 인한 희망도서 제한 2021-11-29 YUNHAESU
-		if(homepage.getHomepage_id().equals("h45")) {
-			service.alertMessage("2021년 희망도서 신청이 마감되었습니다.", request, response);
-			return null;
-		}
+//		if(homepage.getHomepage_id().equals("h45")) {
+//			service.alertMessage("2021년 희망도서 신청이 마감되었습니다.", request, response);
+//			return null;
+//		}
 
 		Member member = getSessionMemberInfo(request);
 		if (!isLogin(request) || !"HOMEPAGE".equals(getSessionMemberLoginType(request))) {
