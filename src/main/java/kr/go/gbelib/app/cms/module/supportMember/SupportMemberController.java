09e7c3cd1d310ec2ad6bd2517bd3a041813d2ae1
hwani <hwani@whalesoft_hwan>
2022-12-22 19:52:58 +0900

    기관명 한글만 입력가능한것 주석처리

--- src/main/java/kr/go/gbelib/app/cms/module/supportMember/SupportMemberController.java
+++ src/main/java/kr/go/gbelib/app/cms/module/supportMember/SupportMemberController.java
@@ -74,7 +74,7 @@ public class SupportMemberController extends BaseController {
     			ValidationUtils.rejectIfEmpty(result, "member_password", "비밀번호를 입력하세요");
     			ValidationUtils.rejectIfEmpty(result, "password_check", "비밀번호 확인을 입력하세요");
     		}
-    		ValidationUtils.rejectOnlyKor(result, "school_name", "학교명은 한글만 입력할 수 있습니다.");
+    		//ValidationUtils.rejectOnlyKor(result, "school_name", "학교명은 한글만 입력할 수 있습니다.");
     		ValidationUtils.rejectOnlyEngNum(result, "member_id", "아이디는 영문/숫자만 사용하실 수 있습니다.");
     		
     		if(supportMember.getMember_id().length() > 10) {
