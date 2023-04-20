8c6c63d6db57e4502b76404d679a13ebb20ca242
안경만 <rudaks012@naver.com>
2023-04-20 13:29:52 +0900

    Feat :[대구]2.28기념학생도서관 최고관리자 학교도서관 회원인증 팝업 문의 URL : http://we.infoset.co.kr/board/boardadmin.php?mode=view&bbs_idx=2&cate=1&search_index=&search_word=&no=4602&vnum=5&page=1
    수정 사항 : 1. 최고 관리자의 경우 조회 및 삭제 기능 추가

--- src/main/java/kr/co/whalesoft/app/board/BoardController.java
+++ src/main/java/kr/co/whalesoft/app/board/BoardController.java
@@ -803,8 +803,16 @@ public class BoardController extends BaseController {
 		SupportMember loginSupport = sessionLoginSupport(request);
 		boolean supportAdmin = false;
 		boolean supportAuth = false;
+		boolean isSiteAdmin = false;
+
+		try {
+			isSiteAdmin = (Boolean) model.asMap().get("authMBA");
+		} catch (Exception e) {
+			isSiteAdmin = false;
+		}
+
 		if(manageCompareIdx(board.getManage_idx(), 212, 213, 224, 225, 226, 227, 228, 230, 281)) {
-			if ( loginSupport == null && !getSessionIsAdmin(request)) {
+			if ( loginSupport == null && !getSessionIsAdmin(request) && !isSiteAdmin ) {
 	    		board.setBefore_url(String.format("/%s/board/index.do?menu_idx=%s%%26manage_idx=%s", homepage.getContext_path(), board.getMenu_idx(), board.getManage_idx()));
 	    		service.alertMessageAndUrl("학교도서관 회원인증 후 이용가능합니다.", String.format("/%s/module/supportMember/index.do?menu_idx=%s&before_url=%s", homepage.getContext_path(), board.getMenu_idx(), board.getBefore_url()), request, response);
 	    		return null;
