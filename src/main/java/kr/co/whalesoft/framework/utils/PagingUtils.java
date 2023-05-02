85b6603fccb564f47047d97ad5c7723fcce2fff8
안경만 <rudaks012@naver.com>
2023-05-02 10:54:42 +0900

    Chore : 공통 모듈 생성으로 기존 변경사항 원복

--- src/main/java/kr/co/whalesoft/framework/utils/PagingUtils.java
+++ src/main/java/kr/co/whalesoft/framework/utils/PagingUtils.java
@@ -262,8 +262,6 @@ public class PagingUtils extends BeanUtils {
 	public void setTotalDataCount(int totalDataCount) {
 		this.totalDataCount = totalDataCount;
 		pagingLogic();
-		map.put("pagingTop", getPagingTop());
-		map.put("pagingBottom", getPagingBottom());
 	}
 
 	public int getStartPageNum() {
