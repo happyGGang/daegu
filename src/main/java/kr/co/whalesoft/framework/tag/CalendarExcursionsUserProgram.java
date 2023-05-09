50f03843fd33020f5764263bcb1d9e3d9e04cce4
sd97428 <yunhaesu.dev@gmail.com>
2023-05-09 17:26:39 +0900

    [대구-안심도서관] 견학신청 관련 문의

--- src/main/java/kr/co/whalesoft/framework/tag/CalendarExcursionsUserProgram.java
+++ src/main/java/kr/co/whalesoft/framework/tag/CalendarExcursionsUserProgram.java
@@ -119,6 +119,19 @@ public class CalendarExcursionsUserProgram extends BodyTagSupport {
 						}
 					}
 					if (flag) {
+						if ("h60".equals(excursions.getHomepage_id())){
+							if (excursions.getApply_yn().equals("Y") && excursions.getClosed_day() == 1 && (now.compareTo(planDate) <= 0 || DateUtils.isSameDay(now, planDate))) {
+								if (maxApplyCount == 0) {
+									sb.append("<a href=\"\" class=\"\" id=\"apply\" keyValue=\"" + excursions.getExcursions_idx() + "\" keyValue2=\"" + plan_date + "\" keyValue3=\"" + excursions.getDate_type() + "\"><span style=\"type-r\"><i></i><em>신청하기</em></span></a><br>");
+								} else {
+									if (maxApplyCount > curApplyCount) {
+										sb.append("<a href=\"\" class=\"\" id=\"apply\" keyValue=\"" + excursions.getExcursions_idx() + "\" keyValue2=\"" + plan_date + "\" keyValue3=\"" + excursions.getDate_type() + "\"><span style=\"type-r\"><i></i><em>신청하기</em></span></a><br>");
+									} else {
+										sb.append("<a href=\"#\">신청 정원 마감</a>");
+									}
+								}
+							}
+						}
 						if (excursions.getApply_yn().equals("Y") && excursions.getClosed_day() == 0 && (now.compareTo(planDate) <= 0 || DateUtils.isSameDay(now, planDate))) {
 							if (maxApplyCount == 0) {
 								sb.append("<a href=\"\" class=\"\" id=\"apply\" keyValue=\"" + excursions.getExcursions_idx() + "\" keyValue2=\"" + plan_date + "\" keyValue3=\"" + excursions.getDate_type() + "\"><span style=\"type-r\"><i></i><em>신청하기</em></span></a><br>");
