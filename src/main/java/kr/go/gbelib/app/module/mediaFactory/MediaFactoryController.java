403ca627d648b944b10158487802ae6449e18623
sd97428 <yunhaesu.dev@gmail.com>
2023-07-10 10:04:00 +0900

    미디어창작소 신청 오류 수정

--- src/main/java/kr/go/gbelib/app/module/mediaFactory/MediaFactoryController.java
+++ src/main/java/kr/go/gbelib/app/module/mediaFactory/MediaFactoryController.java
@@ -368,7 +368,7 @@ public class MediaFactoryController extends BaseController {
 
 				if (mediaFactory.getHomepage_id().equals("h50")){
 					if ( mediaFactory.getMax_apply_person() > 0 ) {
-						if (mediaFactory.getMax_apply_person() <= apply.getPersonnel() ) {
+						if (mediaFactory.getMax_apply_person() < apply.getPersonnel() ) {
 							res.setValid(false);
 							res.setMessage("신청가능 방문인원이 가득찼습니다.");
 							return res;
