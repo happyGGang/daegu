4ba0738c78b81e4701971189164c9330a4c73466
sd97428 <yunhaesu.dev@gmail.com>
2023-08-23 15:26:24 +0900

    sms 전송시 부산도서관으로 나타나는 현상 수정

--- src/main/java/kr/go/gbelib/app/cms/module/circlesRoom/CirclesRoomService.java
+++ src/main/java/kr/go/gbelib/app/cms/module/circlesRoom/CirclesRoomService.java
@@ -84,15 +84,15 @@ public class CirclesRoomService extends BaseService {
 
 			switch (circlesRoom.getStatus()) {
 				case 0 :
-					sms_msg = "[%s] 신청완료(부산도서관)";
+					sms_msg = "[%s] 신청완료(대구도서관)";
 					librarySearch.setUserkey(sessionMemberInfo.getRec_key());
 					break;
 				case 1 :
-					sms_msg = "[%s] 승인완료(부산도서관)";
+					sms_msg = "[%s] 승인완료(대구도서관)";
 					librarySearch.setUserkey(circlesRoom.getRec_key());
 					break;
 				case 2 :
-					sms_msg = "[%s] 미승인(부산도서관)";
+					sms_msg = "[%s] 미승인(대구도서관)";
 					librarySearch.setUserkey(circlesRoom.getRec_key());
 					break;
 				default:
