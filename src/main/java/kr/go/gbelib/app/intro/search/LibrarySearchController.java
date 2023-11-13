3a0cfffae19936cb99372be6207697dc3175e40b
hwani <hwani6865@gmail.com>
2023-11-13 11:08:28 +0900

    검색대 제한해제

--- src/main/java/kr/go/gbelib/app/intro/search/LibrarySearchController.java
+++ src/main/java/kr/go/gbelib/app/intro/search/LibrarySearchController.java
@@ -146,7 +146,7 @@ public class LibrarySearchController extends BaseController {
 				Map<String, Object> result = new HashMap<String, Object>();
 				
 				// 자료실 제외 코드 : [두류]보존서고(1,2,3)
-				librarySearch.setNotShelfCode("AB08,AB09,AB38,AB10,BW06,BW08,BW11,BW12,BW16,BW18,BW19,BW20,BW21,BW22,BW23,BW24,BW25,BW26,AH17,CB16,CB17");
+				librarySearch.setNotShelfCode("AB08,AB09,AB38,AB10,BW06,BW08,BW11,BW12,BW16,BW18,BW19,BW20,BW21,BW22,BW23,BW24,BW25,BW26,AH17,CB17");
 
 				if ( librarySearch.getBooktype().equals("BOOK") ) {
 					result = LibSearchAPI.getBookDetail(librarySearch);
