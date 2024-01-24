2dd958f1040d5a58e9143ccd3419e5cfa1bf3393
hwani <hwani6865@gmail.com>
2024-01-24 10:44:42 +0900

    Revert "Fix : 사물함 관리, 이달의 행사  (회원, 비회원 권한 Check)"
    This reverts commit 1bb30c0655a5a9a9cae60deb359bce07018358d5.

--- src/main/java/kr/go/gbelib/app/module/calendarManage/CalendarManageController.java
+++ src/main/java/kr/go/gbelib/app/module/calendarManage/CalendarManageController.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.co.whalesoft.app.cms.homepage.HomepageService;
-import kr.co.whalesoft.framework.exception.AuthException;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -68,7 +67,6 @@ public class CalendarManageController extends BaseController {
 	@RequestMapping(value = {"/index.*"})
 	public String index(Model model, CalendarManage calendarManage, HttpServletRequest request) throws Exception {
 		Homepage homepage = (Homepage) request.getAttribute("homepage");
-		checkAuth("R", model, request);
 
 		if ((homepage.getHomepage_id().equals("h37") || homepage.getHomepage_id().equals("h49") || homepage.getHomepage_id().equals("h45") || homepage.getHomepage_id().equals("h53"))) {
 			Homepage h = new Homepage();
@@ -112,7 +110,7 @@ public class CalendarManageController extends BaseController {
 	@RequestMapping(value = {"/index_list.*"})
 	public String indexList(Model model, CalendarManage calendarManage, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		Homepage homepage = (Homepage) request.getAttribute("homepage");
-		checkAuth("R", model, request);
+
 		if ((homepage.getHomepage_id().equals("h37") || homepage.getHomepage_id().equals("h49") || homepage.getHomepage_id().equals("h45") || homepage.getHomepage_id().equals("h53"))) {
 			Homepage h = new Homepage();
 			h.setHomepage_id(homepage.getHomepage_id());
@@ -154,12 +152,11 @@ public class CalendarManageController extends BaseController {
 	}
 
 	@RequestMapping(value = {"/edit.*"})
-	public String edit(Model model, CalendarManage calendarManage, HttpServletRequest request) throws AuthException {
+	public String edit(Model model, CalendarManage calendarManage, HttpServletRequest request) {
 		Homepage homepage = (Homepage) request.getAttribute("homepage");
-		checkAuth("R", model, request);
+
 		calendarManage.setHomepage_id(homepage.getHomepage_id());
 		if(calendarManage.getEditMode().equals("MODIFY")) {
-			checkAuth("U", model, request);
 			model.addAttribute("calendarManage", service.copyObjectPaging(calendarManage, service.getCalendarManageOne(calendarManage)));
 		} else {
 			model.addAttribute("calendarManage", calendarManage);
