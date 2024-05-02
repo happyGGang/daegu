4e20146c68d7deec55de5ba85befe00dacd492c2
안경만 <rudaks012@naver.com>
2024-05-02 20:23:04 +0900

    Fix : [대구 안심]동구 검색대 검색제외 자료실 처리요청 URL : http://we.infoset.co.kr/board/boardadmin.php?mode=view&bbs_idx=2&cate=1&search_index=&search_word=&no=6305&vnum=4&page=1 수정 사항 :  동구 검색대 자료실 제외

--- src/main/java/kr/go/gbelib/app/intro/search/LibrarySearchController.java
+++ src/main/java/kr/go/gbelib/app/intro/search/LibrarySearchController.java
@@ -16,6 +16,7 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.time.DateUtils;
+import org.aspectj.weaver.ast.Not;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -142,7 +143,7 @@ public class LibrarySearchController extends BaseController {
 				Map<String, Object> result = new HashMap<String, Object>();
 				
 				// 자료실 제외 코드 : [두류]보존서고(1,2,3)
-				librarySearch.setNotShelfCode("AB08,AB09,AB10,AB38,BW06,BW08,BW11,BW12,BW16,BW18,BW19,BW20,BW21,BW22,BW23,BW24,BW25,BW26,AH17,CB17,FM05,GU04,FP05,GX05,GR06,GW03,AB39,AE59,AF51,AF52");
+				librarySearch.setNotShelfCode(NotShelfCodes.getNotShelfCode());
 
 				if ( librarySearch.getBooktype().equals("BOOK") ) {
 					result = LibSearchAPI.getBookDetail(librarySearch);
@@ -208,7 +209,7 @@ public class LibrarySearchController extends BaseController {
 			librarySearch.setLibraryCodes(libraryCodes);
 		}
 		
-		librarySearch.setNotShelfCode("AB08,AB09,AB10,AB38,BW06,BW08,BW11,BW12,BW16,BW18,BW19,BW20,BW21,BW22,BW23,BW24,BW25,BW26,AH17,CB17,FM05,GU04,FP05,GX05,GR06,GW03,AB39,AE59,AF51,AF52");
+		librarySearch.setNotShelfCode(NotShelfCodes.getNotShelfCode());
 
 		if (StringUtils.isNotEmpty(librarySearch.getBooktype())) {
 			Map<String, Object> result = new HashMap<String, Object>();
---
+++ src/main/java/kr/go/gbelib/app/intro/search/NotShelfCodes.java
@@ -0,0 +1,20 @@
+package kr.go.gbelib.app.intro.search;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public enum NotShelfCodes {
+    AB08, AB09, AB10, AB38, BW06, BW08, BW11, BW12, BW16, BW18,
+    BW19, BW20, BW21, BW22, BW23, BW24, BW25, BW26, AH17, CB17,
+    FM05, GU04, FP05, GX05, GR06, GW03, AB39, AE59, AF51, AF52,
+    GR02, GR03, GR04, GS02, GS03, GT02, GT03, FP02, FP03, FP04,
+    GW01, GU02, GU03, GV02, GV03, FL02, FL03, FL04, FM02, FM03,
+    FM04, GY02, GY03, GX02, GX03, GX04;
+
+    public static String getNotShelfCode() {
+        return Arrays.stream(NotShelfCodes.values())
+                     .map(Enum::name)
+                     .collect(Collectors.joining(","));
+    }
+
+}
