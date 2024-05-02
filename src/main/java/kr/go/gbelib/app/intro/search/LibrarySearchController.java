60b2d9fa139b990653951e4841f3aed75399c4f9
안경만 <rudaks012@naver.com>
2024-05-02 19:43:31 +0900

    Fix : [대구 원고개]홈페이지 및 검색대 검색가능 자료실 추가요청 URL : http://we.infoset.co.kr/board/boardadmin.php?mode=view&bbs_idx=2&cate=1&search_index=&search_word=&no=6253&vnum=4&page=1 수정 사항 : 원고개 도서관 홈페이지, 검색대 조회되도록 수정

--- src/main/java/kr/go/gbelib/app/intro/search/LibrarySearchController.java
+++ src/main/java/kr/go/gbelib/app/intro/search/LibrarySearchController.java
@@ -142,7 +142,7 @@ public class LibrarySearchController extends BaseController {
 				Map<String, Object> result = new HashMap<String, Object>();
 				
 				// 자료실 제외 코드 : [두류]보존서고(1,2,3)
-				librarySearch.setNotShelfCode("AB08,AB09,AB10,AB38,BN13,BW06,BW08,BW11,BW12,BW16,BW18,BW19,BW20,BW21,BW22,BW23,BW24,BW25,BW26,AH17,CB17,FM05,GU04,FP05,GX05,GR06,GW03,AB39,AE59,AF51,AF52");
+				librarySearch.setNotShelfCode("AB08,AB09,AB10,AB38,BW06,BW08,BW11,BW12,BW16,BW18,BW19,BW20,BW21,BW22,BW23,BW24,BW25,BW26,AH17,CB17,FM05,GU04,FP05,GX05,GR06,GW03,AB39,AE59,AF51,AF52");
 
 				if ( librarySearch.getBooktype().equals("BOOK") ) {
 					result = LibSearchAPI.getBookDetail(librarySearch);
@@ -208,7 +208,7 @@ public class LibrarySearchController extends BaseController {
 			librarySearch.setLibraryCodes(libraryCodes);
 		}
 		
-		librarySearch.setNotShelfCode("AB08,AB09,AB10,AB38,BN13,BW06,BW08,BW11,BW12,BW16,BW18,BW19,BW20,BW21,BW22,BW23,BW24,BW25,BW26,AH17,CB17,FM05,GU04,FP05,GX05,GR06,GW03,AB39,AE59,AF51,AF52");
+		librarySearch.setNotShelfCode("AB08,AB09,AB10,AB38,BW06,BW08,BW11,BW12,BW16,BW18,BW19,BW20,BW21,BW22,BW23,BW24,BW25,BW26,AH17,CB17,FM05,GU04,FP05,GX05,GR06,GW03,AB39,AE59,AF51,AF52");
 
 		if (StringUtils.isNotEmpty(librarySearch.getBooktype())) {
 			Map<String, Object> result = new HashMap<String, Object>();
