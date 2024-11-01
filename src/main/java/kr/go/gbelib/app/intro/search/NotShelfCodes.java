e18478e445fe08cb703a20a7744121485b81725e
hwan <hwani6865@whalesoft.co.kr>
2024-11-01 16:18:15 +0900

    Feat : 내집앞도서관 특정 자료실 내집앞도서관예약 버튼제거 요청(내집앞도서관 홈페이지에서는 자료조회X, 동구도서관홈페이지에서는 자료조회O)

--- src/main/java/kr/go/gbelib/app/intro/search/NotShelfCodes.java
+++ src/main/java/kr/go/gbelib/app/intro/search/NotShelfCodes.java
@@ -9,7 +9,7 @@ public enum NotShelfCodes {
     FM05, GU04, FP05, GX05, GR06, GW03, AB39, AE59, AF51, AF52,
     GR02, GR03, GR04, GS02, GS03, GT02, GT03, FP02, FP03, FP04,
     GW01, GU02, GU03, GV02, GV03, FL02, FL03, FL04, FM02, FM03,
-    FM04, GY02, GY03, GX02, GX03, GX04, FG08, CA18;
+    FM04, GY02, GY03, GX02, GX03, GX04, FG08;
 
     public static String getNotShelfCode() {
         return Arrays.stream(NotShelfCodes.values())
