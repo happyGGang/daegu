7b3133e7685092954387469645c484cdec4dcff4
yangbaetsu <yangbaetsu@gmail.com>
2025-03-05 11:35:30 +0900

    Fix : [오류신고][대구 군위]견학신청 비회원 신청시 404에러 Url : http://we.infoset.co.kr/board/boardadmin.php?mode=view&bbs_idx=2&cate=1&search_index=&search_word=&no=10017&vnum=6791&page=1 수정사항 : 군위도서관 - 견학신청 > 비회원이 신청시 404페이지 오류 수정

--- src/main/java/kr/go/gbelib/app/module/excursions/ExcursionsController.java
+++ src/main/java/kr/go/gbelib/app/module/excursions/ExcursionsController.java
@@ -147,7 +147,7 @@ public class ExcursionsController extends BaseController {
 
 		if ( !isLogin(request) && request.getSession().getAttribute("certMember") == null) {
 			if ("h94".equals(homepage.getHomepage_id())) {
-				service.alertMessageAndUrl("본인인증 후 신청가능합니다.", String.format("module/excursions/cert.do?menu_idx=%s&editMode=ADD&excursions_idx=%d&homepage_id=%s", apply.getMenu_idx(), apply.getExcursions_idx(), apply.getHomepage_id()), request, response);
+				service.alertMessageAndUrl("본인인증 후 신청가능합니다.", String.format("/%s/module/excursions/cert.do?menu_idx=%s&editMode=ADD&excursions_idx=%d&homepage_id=%s", homepage.getContext_path(), apply.getMenu_idx(), apply.getExcursions_idx(), apply.getHomepage_id()), request, response);
 			} else if ("h77".equals(apply.getHomepage_id()) || "h37".equals(homepage.getHomepage_id())) {
 				service.alertMessageAndUrl("로그인 후 이용가능합니다.", String.format("/%s/intro/login/index.do?menu_idx=%s&before_url=%s", homepage.getContext_path(), apply.getMenu_idx(), apply.getBefore_url()), request, response);
 			} else {
